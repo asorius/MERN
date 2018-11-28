e8ae0a49c8c224d4b83d83cceb98f6f3c2350c3f
ddexta <18assdd@gmail.com>
2018-11-28 12:43:25 +0000

    created profile model and route to get current profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
-// GET api/profile/test | desc: tests profile route | acces: public
-router.get('/test', (req, res) => {
-  res.json({ msg: 'Profile Works' });
-});
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// GET api/profile | desc: get current profile route | acces: private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(200).json(profile);
+      })
+      .catch(e => res.send(404).json(e));
+  }
+);
 //
 module.exports = router;
