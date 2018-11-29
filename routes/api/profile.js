1d29a0c9e8eeaf0468521f08bdfa99e8593a1f76
ddexta <18assdd@gmail.com>
2018-11-29 12:37:18 +0000

    added create/update profile route with input fields validation as well

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const validateProfileInput = require('../../validation/profile');
 
 // GET api/profile | desc: get current profile route | acces: private
 router.get(
@@ -12,14 +13,78 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar'])
       .then(profile => {
         if (!profile) {
           errors.noprofile = 'There is no profile for this user';
           return res.status(404).json(errors);
         }
-        res.json(200).json(profile);
+        res.json(profile);
       })
       .catch(e => res.send(404).json(e));
+  }
+);
+//
+
+// POST api/profile | desc: create or edit user profile | acces: private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    //Input fields validation
+    const { errors, isValid } = validateProfileInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    //
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    // Skilles needs to be split into array because they in coma separated string
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
+    }
+    //
+    //Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    //
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        //if there is profile ,so this means user is being updated
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        //Create
+        //check for handle of that name to not duplicate handles
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = 'that handle already exists';
+            res.status(400).json(errors);
+          }
+          //Save new profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+    //
   }
 );
 //
